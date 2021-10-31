f50089a1cd26c4ec299bf85f75eb3af8c718c12b
노승은 <seun2230@gmail.com>
2021-10-31 18:11:57 +0900

    SQL의 기본 5문제 풀기(1)

---
+++ 02-SQL-basic.sql
@@ -0,0 +1,22 @@
+-- 02. sql 기본
+-- #1. 데이터 제어어
+-- 정답: 4번
+-- 데이터 제어어 GRANT, REVOKE
+
+-- #2. 데이터 정의어
+-- 정답: 2번
+-- 데이터 정의어 종류는 CREATE, ALTER, DROP, RENAME이 있음.
+
+-- #3. 데이터 제어어
+-- 내 답안 : GRANT, REVOKE
+-- 해설 답안: TCL(데이터 제어어)
+
+-- #4. 1번
+-- 어떻게 접근은 DCL임.
+
+-- #5. 
+-- 정답: 1번 , 2번
+-- 보기 1> : SELECT는 DML 중 하나이다.
+-- 2>: COMMIT는 TCL에 속하며 ROLLBACK.
+-- 3>: DROP은 DDL
+-- 4>: ALTER은 DDL
